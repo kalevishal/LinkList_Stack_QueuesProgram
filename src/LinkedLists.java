7a2b3e2ebf87391965fde6c40d2aad3022e819a2
kalevishal <kalevishal1610@gmail.com>
2021-09-28 21:00:19 +0530

    Adding: Adding elements at start in linked list

--- src/LinkedLists.java
+++ src/LinkedLists.java
@@ -1,18 +1,28 @@
-
 public class LinkedLists {
-	public static void main(String[] args) {
-        Node firstNode = new Node(56);
-        Node secondNode = new Node(30);
-        Node thirdNode = new Node(70);
-        Node head = firstNode;
-        firstNode.next = secondNode;
-        secondNode.next = thirdNode;
-        Node tail = thirdNode;
+    Node head;
+    Node tail;
+    public void push(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            Node temp = head;
+            this.head = newNode;
+            newNode.next = temp;
+        }
+    }
 
-        Node temp = head;
-        while (temp != null) {
-            System.out.println(temp.data);
-            temp = temp.next;
+    public void print() {
+        if (head == null) {
+            System.out.println("Linked List is Empty");
+        } else {
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data+ " ");
+                temp = temp.next;
+            }
+            System.out.println();
         }
     }
 }
---
+++ src/Operations.java
@@ -0,0 +1,9 @@
+public class Operations {
+    public static void addElementAtStart(){
+        LinkedLists linkedList= new LinkedLists();
+        linkedList.push(70);
+        linkedList.push(30);
+        linkedList.push(56);
+        linkedList.print();
+    }
+}
